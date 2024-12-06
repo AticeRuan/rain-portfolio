0fd1ce4671fce6b5bf58861f770de6e5e37f5115
AticeRuan <104746446+AticeRuan@users.noreply.github.com>
2024-12-07 01:42:25 +1300

    minor fix

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-if (!process.env.MONGODB_URI) {
+if (!process.env.NEXT_PUBLIC_MONGODB_URI) {
   throw new Error(
     'Please define the MONGODB_URI environment variable inside .env',
   )
