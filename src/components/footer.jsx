5a8e18e97a2d3daafa8e6783c6ae282851cdbec0
AticeRuan <104746446+AticeRuan@users.noreply.github.com>
2025-01-24 12:27:32 +1300

    updated words in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   const year = date.getFullYear()
   return (
     <div className=" flex items-center justify-center px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-sm my-10 ">
-      Copyright © Rain Ruan {year}
+      © {year} Rain Ruan @ Chill Otters. All rights reserved.
     </div>
   )
 }
