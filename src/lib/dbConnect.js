3cdcfad35e30db79d4d880f326e038dc6df142f4
AticeRuan <104746446+AticeRuan@users.noreply.github.com>
2026-08-15 13:29:07 +1200

    Sync MongoDB environment variable naming

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose'
 
-if (!process.env.NEXT_PUBLIC_MONGODB_URI) {
+if (!process.env.MONGODB_URI) {
   throw new Error(
     'Please define the MONGODB_URI environment variable inside .env',
   )
 }
 
-const MONGODB_URI = process.env.NEXT_PUBLIC_MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI
 
 /**
  * Global is used here to maintain a cached connection across hot reloads
